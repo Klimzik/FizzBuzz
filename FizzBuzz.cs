cb0f1f5e25f70b9b1174ec7189cd1c1f2f93c95d
Mateusz Klimza <mateusz.klimza@idealogic.pl>
2024-03-01 08:17:30 +0100

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class FizzBuzz
+    {
+        public int maxNumber { get; set; }
+        public FizzBuzz() 
+        {
+            Console.WriteLine("Podaj górny zakres liczb\n");
+            maxNumber = Convert.ToInt32(Console.ReadLine());
+            display();
+        }
+
+        public void display() 
+        {
+            for (int i = 1; i <= maxNumber; i++) 
+            {
+                if ( i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                    continue;
+                }
+                if (i % 5 == 0 && i % 3 != 0)
+                {
+                    Console.WriteLine("Buzz");
+                    continue;
+                }
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
